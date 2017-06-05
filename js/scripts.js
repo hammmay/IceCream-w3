96e0d240833440534d63ec76f73786b99f0288a4
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-05 15:25:05 -0700

    amend the scripts for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,20 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var monthInput = $("input#month").val();
-    var colorInput = $("input#color").val();
-    var animalInput= $("input#animal").val();
-    var browserInput = $("input#browser").val();
+    var firstInput = $("input#first").val();
+    var secondInput = $("input#second").val();
+    var thirdInput= $("input#third").val();
 
-    var formArray = [monthInput, colorInput, animalInput, browserInput];
-//create an array and add 4 items to array
+    var formArray = [firstInput, secondInput, thirdInput];
+//create an array and add 3 items to array
 
-    console.log(formArray[1],formArray[0],formArray[2]);
-//shows and pulls the order results in the console, color, month, and animal, only reorders for the sake of the exercise
+    console.log(formArray);
+//simply shows the array in the consule
 
-    var newArrayorder = [];
-//defines an empty new array
-    newArrayorder.push(formArray[1]);
-    newArrayorder.push(formArray[0]);
-    newArrayorder.push(formArray[2]);
-//lists out the 3 elements and appends them with the .push
-    console.log(newArrayorder);
-//shows the new array in the consule
-    $("#list").append("<li>" + newArrayorder[0] + "</li>");
-    $("#list").append("<li>" + newArrayorder[1] + "</li>");
-    $("#list").append("<li>" + newArrayorder[2] + "</li>");
-//defines the list class, elements, and it's new order from when it was defined by the original array elements
+    formArray.forEach(function(form) {
+      $("#list").append("<li>" + form + "</li>")
+    });
+//puts in a loop to put in an append on the list for each input
+
     event.preventDefault();
   });
 });
